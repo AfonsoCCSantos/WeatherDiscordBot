4c20948f5d84b708a0a2588bde0974c03aaa79ce
AfonsoCCSantos <afonso.acc.santos@gmail.com>
2022-08-28 15:34:38 +0100

    Code refactored in WeatherInCommandHandler

--- WeatherDiscordBot/CommandHandlers/WeatherInCommandHandler.cs
+++ WeatherDiscordBot/CommandHandlers/WeatherInCommandHandler.cs
@@ -1,7 +1,4 @@
 ﻿using Discord.WebSocket;
-using Newtonsoft.Json;
-using WeatherDiscordBot.Models;
-
 namespace WeatherDiscordBot.CommandHandlers
 {
     public class WeatherInCommandHandler : CommandHandler
@@ -9,25 +6,12 @@ namespace WeatherDiscordBot.CommandHandlers
         public override async Task Handle(SocketSlashCommand command, HttpClient httpClient)
         {
             var city = command.Data.Options.First().Value.ToString();
+            var apiKey = Environment.GetEnvironmentVariable("ApiKey");
 
-            string url = $"{Constants.GEOCODING_BASE_URL}q={city}&appid={Environment.GetEnvironmentVariable("ApiKey")}";
+            var geocodingResponse = await httpClient.CallGeocodingAPI(city, apiKey);
+            var openWeatherResponse = await httpClient.CallOpenWeatherAPI(geocodingResponse, apiKey);
 
-            var response = await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Get, url));
-            var trimmedResult = (await response.Content.ReadAsStringAsync())[1..^1];
-
-            var geocodingResponse = JsonConvert.DeserializeObject<GeocodingResponse>(trimmedResult);
-
-            string openWeatherUrl = $"{Constants.OPENWEATHER_BASE_URL}lat={geocodingResponse.lat}&lon={geocodingResponse.lon}" +
-                $"&appid={Environment.GetEnvironmentVariable("ApiKey")}&units=metric";
-
-            var weatherResponse = await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Get, openWeatherUrl));
-            var result = await weatherResponse.Content.ReadAsStringAsync();
-
-            var weatherResponse2 = JsonConvert.DeserializeObject<OpenWeatherResponse>(result);
-
-            await command.RespondAsync($"Temperature in {city} is {weatherResponse2.main.temp}");
-
-            response.Dispose();
+            await command.RespondAsync($"Temperature in {city} is {openWeatherResponse.main.temp}");
         }
     }
 }
---
+++ WeatherDiscordBot/WeatherExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using WeatherDiscordBot.Models;
+
+namespace WeatherDiscordBot
+{
+    public static class WeatherExtensions
+    {
+        public static async Task<GeocodingResponse> CallGeocodingAPI(this HttpClient httpClient, string city, string apiKey)
+        {
+            string url = $"{Constants.GEOCODING_BASE_URL}q={city}&appid={apiKey}";
+            var response = await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Get, url));
+            var trimmedResult = (await response.Content.ReadAsStringAsync())[1..^1];
+            var geocodingResponse = JsonConvert.DeserializeObject<GeocodingResponse>(trimmedResult);
+
+            return await Task.FromResult(geocodingResponse);
+        }
+
+        public static async Task<OpenWeatherResponse> CallOpenWeatherAPI(this HttpClient httpClient, 
+                                                                         GeocodingResponse geocodingResponse, string apiKey)
+        {
+            string url = $"{Constants.OPENWEATHER_BASE_URL}lat={geocodingResponse.lat}&lon={geocodingResponse.lon}" +
+                $"&appid={apiKey}&units=metric";
+            var response = await httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Get, url));
+            var result = await response.Content.ReadAsStringAsync();
+            var openWeatherResponse = JsonConvert.DeserializeObject<OpenWeatherResponse>(result);
+
+            return await Task.FromResult(openWeatherResponse);
+        }
+
+    }
+}
